e3868b0fc7e54a0a7224ddd2fe862df73aae037a
Ercan Atak <atakee@gmail.com>
2026-08-26 01:14:43 +0200

    Lehrjahre wing: first two faces, GoneWithTheTailwind and Finance_Logger

---
+++ lehrjahre/finance-logger/app.js
@@ -0,0 +1,23 @@
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+const form = document.querySelector(".new-item-form");
+// console.log(form.children);
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#toFrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
+});
